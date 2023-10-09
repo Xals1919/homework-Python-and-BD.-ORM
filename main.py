71a44a268be8e577a9e9c03ba0ec4cb74edd1d71
Viktor <viktor.tretyakov.91@gmail.com>
2023-10-09 15:27:03 +0300

    "new commit"

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #Импортируем необходимые библиотеки
 import json
 import sqlalchemy
+from datetime import datetime
 from sqlalchemy.orm import sessionmaker
 from models import create_tables, Publisher, Book, Shop, Stock, Sale
+from pprint import pprint
 
 #data source name
 DSN = 'postgresql://postgres:lakI1003@localhost:5432/homeworkORM'
@@ -30,8 +32,22 @@ for n in data:#Перебираем переменную
     session.add(model(id=n.get('pk'), **n.get('fields')))
 session.commit()
 
+def get_shop(namepublisher):
+    all_tables = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).\
+        join(Sale.stock).\
+        join(Stock.shop).\
+        join(Stock.book).\
+        join(Book.publisher)
+    if namepublisher.isdigit():
+        dasd = all_tables.filter(Publisher.id == namepublisher).all()
+    else:
+        dasd = all_tables.filter(Publisher.name == namepublisher).all()
+    for c in dasd:
+        print(f'{c[0]: <40} | {c[1]: <10} | {c[2]: <8} | {datetime.strftime(datetime.strptime(c[3], "%Y-%m-%dT%H:%M:%S.%fZ"), "%d-%m-%Y")}')
+
 #Варианты из json: O’Reilly | Pearson | Microsoft Press | No starch press
-namepublisher = input('Введите Имя автора: ') 
-for c in session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).join(Sale.stock).join(Stock.shop).join(Stock.book).join(Book.publisher).filter(Publisher.name == namepublisher).all():
-    d = c[3].split('T')
-    print(f'{c[0]} | {c[1]} | {c[2]} | {d[0]}')
+if __name__ == '__main__':
+    namepublisher = input('Введите Имя или номер автора: ') 
+    get_shop(namepublisher)
+
+    __pucache__
